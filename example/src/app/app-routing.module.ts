90ff2424b6f499593bd83e2678bfa08ece0aeb38
Noah Prail <nprail@filiosoft.com>
2021-07-12 09:56:35 -0400

    chore: Upgrade example app to Angular 11.2

--- example/src/app/app-routing.module.ts
+++ example/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule]
 })
