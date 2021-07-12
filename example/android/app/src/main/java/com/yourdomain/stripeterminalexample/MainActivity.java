405443ea998268579a680eb08d786faa0352dee1
Noah Prail <nprail@filiosoft.com>
2021-07-12 09:33:26 -0400

    chore: Initial work on upgrading example to Capacitor 3

--- example/android/app/src/main/java/com/yourdomain/stripeterminalexample/MainActivity.java
+++ example/android/app/src/main/java/com/yourdomain/stripeterminalexample/MainActivity.java
@@ -8,16 +8,4 @@ import io.event1.capacitorstripeterminal.StripeTerminal;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-      add(StripeTerminal.class);
-    }});
-  }
-}
+public class MainActivity extends BridgeActivity {}
